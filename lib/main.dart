10cbf69d0cc66773b2b4958a79a43b5619caabd8
kevin pa1000 <64033672+kevinbadboyz@users.noreply.github.com>
2023-12-18 16:49:51 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: ProductHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
